c7f9ab4e7a877a83e4f5b10820171a4c536735fb
Thomas Beckenhauer <tom@beckenhauer.me>
2021-05-01 14:48:51 -0400

    update: meetup scripts

--- Meetup-cleanupNotification-openTabs.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name Meetup - Account Communications - Open Tabs Loop
-// @namespace Violentmonkey Scripts
-// @match https://www.meetup.com/account/comm/
-// @grant none
-// ==/UserScript==
-
-$('.list-item a').forEach((i) => {window.open(i.href, "_blank")})
-
